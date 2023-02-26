b1b75ce7a20ff1b5b284d11ecef2342d8558e94e
SY <seyav29@gmail.com>
2023-02-26 13:37:22 -0500

    1st commit

---
+++ test.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "GET, POST");
+  res.header("access-control-allow-headers", "Content-Type");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>ET|O GET</h1>");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Received POST request!");
+});
+
+app.listen(3000, () => {
+  console.log("back works");
+});
+
+
